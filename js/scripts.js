958bb72605a67b381ccffdd9c07ec9223e5e9cce
Jeffrey Ruder <ruderjt@gmail.com>
2016-02-12 13:05:42 -0800

    Implement ability to add multiple pizzas to order

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-function Pizza() {
-  this.pizzaToppings = ["cheese"];
-  this.pizzaSize;
+function Order() {
+  this.pizzas = [];
+}
+
+Order.prototype.addPizza = function (pizza) {
+  var pizzaToAdd = new Pizza(pizza.pizzaId, pizza.pizzaSize);
+  pizza.pizzaToppings.forEach(function(topping) {
+    pizzaToAdd.pizzaToppings.push(topping);
+  });
+  this.pizzas.push(pizzaToAdd);
+};
+
+function Pizza(pizzaId, size) {
+  this.pizzaId = pizzaId;
+  this.pizzaToppings = [];
+  this.pizzaSize = size;
 };
 
 var sizeCosts = {
-  "small": 800,
-  "medium": 1000,
-  "large": 1200
+  "Small": 800,
+  "Medium": 1000,
+  "Large": 1200
 };
 
 var toppingCosts = {
@@ -39,7 +52,9 @@ var formatCost = function(cost) {
 
 //USER INTERFACE
 
-var populateToppings = function() {
+var setupPizzaBuilder = function(pizza) {
+  $(".topping-checkboxes").empty();
+  $(".working-toppings").empty();
   for (var topping in toppingCosts) {
     if (toppingCosts.hasOwnProperty(topping)) {
       $(".topping-checkboxes").append(
@@ -51,46 +66,83 @@ var populateToppings = function() {
       );
     }
   }
-  $("input[value='cheese']").attr("checked", true);
+  $("#" + pizza.pizzaSize).prop("checked", true)
+  $("input[value='cheese']").prop("checked", true);
+  pizza.pizzaToppings.push("cheese");
+  addToppingToPizzaBuilderList("cheese");
+  updateCost(pizza);
+  beginSizeListener(pizza);
+  beginToppingListener(pizza);
 };
 
-var appendToppingToList = function(topping) {
+var beginSizeListener = function(pizza) {
+  $(".pizza-size").change(function() {
+    pizza.pizzaSize = $(this).val();
+    $(".working-size").empty().append(pizza.pizzaSize);
+    updateCost(pizza);
+  });
+}
+
+var beginToppingListener = function(pizza) {
+  $(".pizza-topping").change(function() {
+    var thisTopping = $(this).val();
+    if (pizza.pizzaToppings.indexOf(thisTopping) > -1) {
+      pizza.removeTopping(thisTopping);
+      removeToppingFromPizzaBuilderList(thisTopping);
+      updateCost(pizza);
+    } else {
+      pizza.pizzaToppings.push(thisTopping);
+      addToppingToPizzaBuilderList(thisTopping);
+      updateCost(pizza);
+    }
+  });
+}
+
+var addToppingToPizzaBuilderList = function(topping) {
   $(".working-toppings").append("<li class='working-topping'>" + topping + "</li>");
 };
 
-var removeToppingFromList = function(topping) {
+var removeToppingFromPizzaBuilderList = function(topping) {
   $(".working-topping:contains(" + topping + ")").remove();
-}
+};
+
+var updateCost = function(pizza) {
+  $(".working-cost").empty().append(formatCost(pizza.cost()));
+};
+
+var updateOrderList = function(pizza, order) {
+
+  $(".order-pizzas").empty();
+  for (var eachPizza of order.pizzas) {
+    $(".order-pizzas").append(
+      "<div class='order-pizza'>" +
+        "<strong>" + eachPizza.pizzaSize + "&nbsp;Pizza</strong> " +
+        "<ul class='list-inline order-pizza-toppings' id='toppings"+ eachPizza.pizzaId + "'></ul>" +
+        formatCost(eachPizza.cost()) +
+      "</div>"
+    );
+    for (var topping of eachPizza.pizzaToppings) {
+      $("#toppings" + eachPizza.pizzaId).append("<li class='order-pizza-topping'>" + topping + "</li>");
+    }
+  }
+};
 
 //DOCUMENT READY
 
 $(function() {
 
-  var workingPizza = new Pizza;
-  workingPizza.pizzaSize = "medium";
+  var idCounter = 0;
+  var workingOrder = new Order();
+  var workingPizza = new Pizza(idCounter, "Medium");
+  setupPizzaBuilder(workingPizza);
 
-  populateToppings();
-  appendToppingToList("cheese");
-
-  $(".pizza-size").change(function() {
-    workingPizza.pizzaSize = $(this).val();
-    $(".working-cost").empty().append(formatCost(workingPizza.cost()));
+  $("#add-to-order").on("submit", function(event) {
+    event.preventDefault();
+    console.log("Submission received");
+    workingOrder.addPizza(workingPizza);
+    updateOrderList(workingPizza, workingOrder);
+    idCounter++;
+    workingPizza = new Pizza(idCounter, "Medium");
+    setupPizzaBuilder(workingPizza);
   });
-
-  $(".pizza-topping").change(function () {
-    var thisTopping = $(this).val();
-    console.log(thisTopping);
-    if (workingPizza.pizzaToppings.indexOf(thisTopping) > -1) {
-      workingPizza.removeTopping(thisTopping);
-      removeToppingFromList(thisTopping);
-      $(".working-cost").empty().append(formatCost(workingPizza.cost()));
-    } else {
-      workingPizza.pizzaToppings.push(thisTopping);
-      console.log(workingPizza.pizzaToppings);
-      appendToppingToList(thisTopping);
-      $(".working-cost").empty().append(formatCost(workingPizza.cost()));
-    }
-  });
-
-
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,18 @@
 describe('Pizza', function() {
 
   it("has a size", function() {
-    var testPizza = new Pizza();
-    testPizza.pizzaSize = "large";
-    expect(testPizza.pizzaSize).to.equal("large");
+    var testPizza = new Pizza(1, "Large");
+    expect(testPizza.pizzaSize).to.equal("Large");
   });
 
   it("receives toppings and returns them", function() {
-    var testPizza = new Pizza();
+    var testPizza = new Pizza(1, "Large");
     testPizza.pizzaToppings.push("anchovies");
-    expect(testPizza.pizzaToppings[1]).to.equal("anchovies");
+    expect(testPizza.pizzaToppings[0]).to.equal("anchovies");
   });
 
   it("returns total cost based on size and toppings", function() {
-    var testPizza = new Pizza();
-    testPizza.pizzaSize = "large";
+    var testPizza = new Pizza(1, "Large");
     testPizza.pizzaToppings.push("anchovies");
     testPizza.pizzaToppings.push("mushrooms");
     testPizza.pizzaToppings.push("pepperoni");
@@ -23,8 +21,7 @@ describe('Pizza', function() {
   });
 
   it("returns a formatted cost for DOM use", function() {
-    var testPizza = new Pizza();
-    testPizza.pizzaSize = "large";
+    var testPizza = new Pizza(1, "Large");
     testPizza.pizzaToppings.push("anchovies");
     testPizza.pizzaToppings.push("mushrooms");
     testPizza.pizzaToppings.push("pepperoni");
@@ -33,24 +30,34 @@ describe('Pizza', function() {
   });
 
   it("allows toppings to be removed", function() {
-    var testPizza = new Pizza();
-    testPizza.pizzaSize = "large";
+    var testPizza = new Pizza(1, "Large");
     testPizza.pizzaToppings.push("anchovies");
     testPizza.pizzaToppings.push("mushrooms");
     testPizza.pizzaToppings.push("pepperoni");
     testPizza.pizzaToppings.push("sausage");
     testPizza.removeTopping("sausage");
     expect(testPizza.cost()).to.equal(1350);
-    expect(testPizza.pizzaToppings.length).to.equal(4);
+    expect(testPizza.pizzaToppings.length).to.equal(3);
   });
 
   it("can have multiple copies of the same topping", function() {
-    var testPizza = new Pizza();
-    testPizza.pizzaSize = "large";
+    var testPizza = new Pizza(1, "Large");
     testPizza.pizzaToppings.push("anchovies");
     testPizza.pizzaToppings.push("anchovies");
-    expect(testPizza.pizzaToppings[2]).to.equal("anchovies");
+    expect(testPizza.pizzaToppings[1]).to.equal("anchovies");
     expect(testPizza.cost()).to.equal(1350);
+  });
+
+});
+
+describe('Order', function() {
+
+  it("contains an array of pizza objects", function() {
+    var testPizza = new Pizza(1, "Large");
+    testPizza.pizzaToppings.push("anchovies");
+    var testOrder = new Order();
+    testOrder.addPizza(testPizza);
+    expect(testOrder.pizzas[0].pizzaToppings[0]).to.equal("anchovies");
   });
 
 });
